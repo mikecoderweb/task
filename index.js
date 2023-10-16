8e0a3c5214be77f68ee5b3cacc6f079a989a302b
mikecoderweb <muydinkhon.abduvaliev04@gmail.com>
2023-10-16 13:47:16 +0500

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 console.log('hey');
 
 console.log('fjjsjd');
+
+function removeLeadingTrailing(n) {
+    n = n.replace(/^0+(?=\d\.)/, '');
+    n = n.replace(/^0+/, '');
+    const parts = n.split('.');
+    if (parts.length === 2) {
+        parts[1] = parts[1].replace(/0+$/, '');
+        if (parts[1] === '') {
+            return parts[0];
+          }
+      
+          return parts.join('.');
+        }
+      
+        return n;
+      }
+      console.log(removeLeadingTrailing("230.000"));
